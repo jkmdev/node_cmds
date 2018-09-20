c9b42baa188801237113100ba0f88dccdf5edc5c
Anne Andy <anneandy@localhost.localdomain>
2018-09-19 23:17:04 -0400

    Make quicknote only append text

--- index.js
+++ index.js
@@ -1,41 +1,29 @@
 #!/usr/bin/env node
 'use strict';
 
-var minimist = require('minimist');
 var fs = require('fs');
 var os = require('os');
 var url = require('url');
 
-console.log(url.URL);
-
-const args = minimist(process.argv.slice(2));
-
-var appendText = args.a;
-var newText = args.n;
-
 var userName = os.userInfo().username;
 
-var fileName = 'QUICKNOTES.txt';
+
+var fileName = 'QUICKNOTE.txt';
 const fileURL = new url.URL('file:///home/' + userName + '/Documents/' + fileName);
+
+var appendText = '';
+
+for (let j = 2; j < process.argv.length; j++) {
+    appendText += process.argv[j] + " ";
+}
 
 if (appendText) {
   appendTextToFile();
-} else if (newText) {
-  createNewFile();
 }
 
 function appendTextToFile() {
-  fs.appendFile(fileURL, appendText + "\n", function (err) {
+  fs.appendFile(fileURL, appendText + '\n', function (err) {
     if (err) throw err;
-    console.log('"' + appendText + '" added to ' + fileName);
+    console.log(appendText + 'added to ' + fileName);
   });
 }
-
-function createNewFile() {
-  fs.writeFile(fileURL, newText, function (err) {
-    if (err) throw err;
-    console.log('New ' + fileName + ' created. "' + newText + '" added to file.');
-  });
-}
-
-//issue-393
