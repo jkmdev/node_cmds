c355f4722a5bb24ba3ad03cf2174314485bed701
Anne Andy <anneandy@localhost.localdomain>
2018-09-30 14:20:18 -0400

    misc ahnge

--- commands/startp.js
+++ commands/startp.js
@@ -8,64 +8,57 @@
 //assumes a Project folder exists in the user's directory
   //in the future the project folder can be set on the command line
 
+var fs = require('fs');
 var fsProm = require('fs').promises;
 var utils = require('../lib/utils.js');
+var readline = require('readline');
 
 const atomCmd = "atom Projects";
 const terminalCmd = "gnome-terminal --working-directory='Projects/Node/NodeCommands'";
 
 function main() {
-  var projectName = getProjectNameFromUser();
-  // var readline = require('readline');
-  //
-  // var rl = readline.createInterface({
-  //   input: process.stdin,
-  //   output: process.stdout,
-  //   terminal: false
-  // });
-  //
-  // rl.on('line', function (cmd) {
-  //   console.log('You just typed: '+cmd);
-  // });
 
-  utils.openApp(atomCmd);
-  utils.openApp(terminalCmd);
+   var project = getProjectNameFromUser();
+   console.log('here');
+    //utils.openApp(atomCmd);
+    //utils.openApp(terminalCmd);
 }
 main();
 
-
-
 function getProjectNameFromUser() {
 
-  fsProm.readdir(utils.urlFromUserDir('Projects'))
-      .then((ret) => {
-        console.log("Select project type: ");
-        //utils.printDir(ret);
-        getUserInput(ret);
-      })
-      .then ((ret) => {
-        console.log('???');
-      })
-      .catch((err) => console.log(err));
+  var dir = fs.readdirSync(utils.urlFromUserDir('Projects'));
+  utils.printDir(dir);
+  var input = getUserInput(dir);
 
+    // fsProm.readdir(utils.urlFromUserDir('Projects'))
+    //     .then((directory) => {
+    //       console.log("Select project type: ");
+    //       getUserInput(directory)
+    //         .then((ret) => resolve(ret));
+    //     })
+    //     .then ((ret) => {
+    //       console.log('???');
+    //     })
+    //     .catch((err) => console.log(err));
 }
 
-function isFolder(element) {
-  return element.indexOf(".") != -1 ? false : true;
-}
+
 
 function getUserInput(directory) {
-  process.stdin.on('readable', () => {
-    const chunk = process.stdin.read();
-    if (chunk !== null && isValidInput(directory, `${chunk}`)) {
-      process.stdout.write(`You entered: ${chunk}`);
-      return true;
-    } else {
-      console.log('Invalid input, try again.');
-      process.stdin.resume();
-    }
-  });
+
+    var rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+      terminal: false
+    });
+
+    rl.on('line', function (cmd) {
+      console.log('You just typed: '+cmd);
+      if (isValidInput(directory, cmd)) return cmd;
+    });
 }
+
 
 function isValidInput(directory, input) {
   var valid = false;
