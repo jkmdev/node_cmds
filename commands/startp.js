c552e412616e15808589aa16f3ea6c85647d4411
Anne Andy <anneandy@localhost.localdomain>
2018-09-30 00:57:01 -0400

    Get input working

--- commands/startp.js
+++ commands/startp.js
@@ -6,24 +6,70 @@ var utils = require('../lib/utils.js');
 const {promisify} = require("es6-promisify");
 var exec = promisify(require('child_process').exec);
 
-fsProm.readdir(utils.urlFromHome('Projects'))
-  .then((ret) => console.log(''))
-  .catch((err) => console.log(err));
+//if projects directory doesn't exist, it creates one
 
-exec("which atom")
-  .then((ret) => openApp(ret));
+fsProm.readdir(utils.urlFromUserDir('Projects'))
+    .then((ret) => {
+      console.log("Select project type: ");
+      //printDir(ret);
+      getUserInput(ret);
+    })
+    .then ((ret) => {
+      console.log('???');
+    })
+    .catch((err) => console.log(err));
 
-var projectName =
+// exec("which atom")
+//   .then((ret) => openApp(ret));
 
-openApp("gnome-terminal --working-directory='Projects/NodeCommands'");
+//var projectName =
+
+//openApp("atom");
+
+//openApp("gnome-terminal --working-directory='Projects/Node/NodeCommands'");
+
+function printDir(directory) {
+  directory.forEach(function(element) {
+    if (isFolder(element)) {
+      console.log("\t" + element);
+    }
+  });
+}
+
+function isFolder(element) {
+  return element.indexOf(".") != -1 ? false : true;
+}
+
+function getUserInput(directory) {
+  process.stdin.on('readable', () => {
+    const chunk = process.stdin.read();
+    if (chunk !== null && isValidInput(directory, `${chunk}`)) {
+      process.stdout.write(`You entered: ${chunk}`);
+      return true;
+    } else {
+      console.log('Invalid input, try again.');
+      process.stdin.resume();
+    }
+  });
+}
+
+function isValidInput(directory, input) {
+  var valid = false;
+  input = input.replace(/\r?\n|\r/g,"");
+  directory.forEach(function(element) {
+    if (element === input) valid = true;
+  });
+  return valid;
+}
 
 function openApp(location) {
-  exec(location);
+  exec(location)
+    .catch((err) => console.log(err));
 }
 
 //enter project folder in command line
 //will open project with atom and two terminals
-
+//when atom closes, the terminals close too
 
 //Starts environment for existing project
 
